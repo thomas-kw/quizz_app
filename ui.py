393f6cb60545dbace1583cfd55bb77d3f91e7b2f
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-08-30 13:43:57 +0900

    questions fixed

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = "Arial", 20, "italic"
@@ -6,7 +7,8 @@ FONT = "Arial", 20, "italic"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -17,6 +19,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some question text",
             fill=THEME_COLOR,
             font=FONT
@@ -31,4 +34,10 @@ class QuizInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
